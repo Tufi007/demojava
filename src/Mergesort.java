66ea0f3bb86d8a2c53a100742b55e913a922de61
Tufi007 <taufeeqr346@gmail.com>
2023-11-23 13:21:00 +0530

    sorting

---
+++ src/Mergesort.java
@@ -0,0 +1,11 @@
+public class Mergesort {
+    public static void main(String[] args) {
+        int[]a={2,3,4};
+        int [] b ={4,5,6,7};
+        sort(a,b);
+    }
+
+    private static void sort(int[] a, int[] b) {
+        
+    }
+}
